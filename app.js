317bedb5fcae6e7f09e439c3a99885690c448b5b
Himansu01-sharma <himansusharma0928@gmail.com>
2025-10-03 01:54:54 +0530

    The Game

--- app.js
+++ app.js
@@ -5,13 +5,52 @@ let compScore =0;
 const choices =  document.querySelectorAll(".choice");
 const genCompChoice =()=>{
     const options =["rock","paper","scissors"];
-    Math.random();
-}
+   const randIdx= Math.floor(Math.random()*3);
+   return options[randIdx];
+};
 
+const drwa =()=>{
+    console.log("its a drwa match ");
+
+};
+
+const ShowWinner=(userWin)=> {
+    if(userWin) {
+        console.log("you win");
+    }
+    else{
+        console.log("you loose");
+    }
+};
 
 const playGame=(choiceid)=>{
-    console.log("user choice=",userchoice);
-     // Generate computer Choice->modular
+    console.log("user choice=",choiceid);
+
+    
+    // Generate computer Choice->modular
+const compchoice=genCompChoice();
+console.log("comp choice=",compchoice);
+if(choiceid===compchoice){
+    drwa(); 
+
+}
+else {
+    let userWin=true;
+    if(choiceid==="rock"){
+        // scissor, paper
+        userWin=compchoice==="paper"?false:true;
+
+    }
+else if(choiceid==="paper"){
+    // rock, scissor
+    userWin=compchoice==="scissor"?false:true;
+}
+else {
+    // paper, rock
+    userWin=compchoice==="rock"?false:true;
+}
+ShowWinner(userWin);
+}
 };
 
 choices.forEach((choice)=>{
